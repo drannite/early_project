2f2009adc8418da487df78682cc267af2bc9b74d
Andrew <apankevicius@gmail.com>
2018-12-24 15:22:30 +1100

    Added login/logout

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ gem 'devise'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.2'
-# Use sqlite3 as the database for Active Record
+
 
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'pages#home'
   get  'about' => 'pages#about'
-  get  'SignUp' => 'devise/registrations#new'
+  get  'signup' => 'registrations#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
